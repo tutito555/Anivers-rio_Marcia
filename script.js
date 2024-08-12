d2b3a82a078e7864d79b3d772e821014a4ec3b0a
tutito555 <arthurribeiro2952@gmail.com>
2024-08-12 11:13:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Botão Instagram
+let Instagram = document.getElementById("Instagram");
+Instagram.addEventListener("click", function () {
+    window.open("https://www.instagram.com/marciamartinsribeiroantunes?igsh=aHkwdzBjcjRvZGVm", "_blank"); // Abre o Instagram em uma nova aba
+});
+
+// Botão Curso em Vídeo
+let CursoEmVideo = document.getElementById("ap");
+CursoEmVideo.addEventListener("click", function () {
+    window.open("https://www.cursoemvideo.com", "_blank"); // Abre o Curso Em Vídeo em uma nova aba
+});
+
+// Botão História
+let historia = document.getElementById("historia");
+historia.addEventListener("click", function () {
+    window.location.href = "historia.html"; // Redireciona para a página historia.html
+});
+
+// Botão Galeria
+let galeria = document.getElementById("galeria");
+galeria.addEventListener("click", function () {
+    window.location.href = "galeria.html"; // Redireciona para a página galeria.html
+});
+
+// Botão Download
+let download = document.getElementById("download");
+download.addEventListener("click", function () {
+    window.open("link do mediafire", "_blank"); // Abre o link de download em uma nova aba
+});
